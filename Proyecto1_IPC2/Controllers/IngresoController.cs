e882a63e688b2ac5ee7f4f1b65e8569971ec4397
Kenneth López <khlopez2000@gmail.com>
2020-09-09 20:29:23 -0600

    Iniciando vistas

---
+++ Proyecto1_IPC2/Controllers/IngresoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto1_IPC2.Controllers
+{
+    public class IngresoController : Controller
+    {
+        public ActionResult Inicio_sesion()
+        {
+            return View();
+        }
+
+        public ActionResult Registro()
+        {
+            return View();
+        }
+    }
+}
